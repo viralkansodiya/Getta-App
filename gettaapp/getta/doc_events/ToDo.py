16fa7aa077d18636a7c9aab140f86caa97b3b1be
viralkansodiya <viral@fosserp.com>
2024-12-17 06:00:37 +0000

    set email trigger for ToDO

--- gettaapp/getta/doc_events/ToDo.py
+++ gettaapp/getta/doc_events/ToDo.py
@@ -1,8 +1,10 @@
 import frappe
 from urllib.parse import urlparse
 from urllib.parse import urlencode, urlunparse
-
+from frappe.utils import get_link_to_form
+from frappe import _
 def validate(self, method):
+    notify_user(self)
     WS = frappe.get_doc("WhatsApp Settings")
     if not WS.enabled:
         return
@@ -38,6 +40,7 @@ def validate(self, method):
                 """
             if reference_doctype and reference_name:
                 send_template(to, reference_doctype, reference_name, message)
+    
 
 
 def send_template(to, reference_doctype, reference_name, message):
@@ -56,3 +59,58 @@ def send_template(to, reference_doctype, reference_name, message):
         doc.save()
     except Exception as e:
         raise e
+    
+def notify_user(self):
+    if self.is_email_sent:
+        return
+    if not self.allocated_to:
+        return
+    if not self.reference_type and not self.reference_name:
+        message = """
+            <body style="background-color: #f8f9fa; padding: 20px;">
+            <div class="container">
+                <div class="card shadow">
+                <div class="card-header bg-success text-white text-center row">
+                    <h1>ToDo Assignment</h1>
+                </div>
+                <div class="card-body row">
+                    <p>Dear <strong>{0}</strong>,</p>
+                    <p>You have been assigned a new task. Please find the details below: {5}</p>
+                    <div class="table-responsive row">
+                    <table class="table table-bordered">
+                        <tbody>
+                        <tr>
+                            <th scope="row">Description</th>
+                            <td>{1}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Assigned By</th>
+                            <td>{2}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Deadline</th>
+                            <td>{3}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                    <p>Please ensure the task is completed by the specified deadline. Feel free to reach out if you have any questions or need further clarification.</p>
+                    <p>Best regards,</p>
+                    <p><strong>{4}</strong></p>
+                </div>
+                <div class="card-footer text-muted text-center row">
+                    <small>This is an automated email. Please do not reply directly to this email.</small>
+                </div>
+                </div>
+            </div>
+
+
+              """.format(
+            self.allocated_to, self.description, self.assigned_by, self.date, frappe.db.get_value("User", self.assigned_by, "full_name"), get_link_to_form("ToDo", self.name)
+        )
+
+        frappe.sendmail(
+			recipients=[self.allocated_to],
+			subject="ToDo Assignment",
+			message = message
+		)
