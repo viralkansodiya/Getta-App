b2e5f63d929095c5c8f18fa459328c194cdfcbf2
viral <viral@fosserp.com>
2025-01-07 07:29:23 +0000

    Whatsapp integration of assignment of lead

--- gettaapp/getta/doc_events/ToDo.py
+++ gettaapp/getta/doc_events/ToDo.py
@@ -1,29 +1,102 @@
 import frappe
-from urllib.parse import urlparse
-from urllib.parse import urlencode, urlunparse
-from frappe.utils import get_link_to_form
+from urllib.parse import quote
+from frappe.utils import get_link_to_form, get_url
 from frappe import _
-import request
+import requests
 
 def validate(self, method):
     notify_user(self)
+    send_whatsapp_message(self)
+
+def send_whatsapp_message(self):
+    wh_setting = frappe.get_doc("Whatsapp Setting")
+    if not wh_setting:
+        return
+    
     if self.allocated_to:
         to =  frappe.db.get_value("User", self.allocated_to, "mobile_no") or frappe.db.get_value("User", self.allocated_to, "phone") 
+        if not to:
+            frappe.throw("Please Update the phone number of the user , {0}".format(get_link_to_form("User", self.allocated_to)))
+        
+        number = validate_phone_number(to, self.allocated_to)
         reference_doctype = self.reference_type
         reference_name = self.reference_name
-        from urllib.parse import quote
+        
+        original_url = f"{get_url()}/app/{reference_doctype}/{reference_name}"
 
-        # Original URL
-        original_url = f"https://web3.gettaproperties.com/app/{reference_doctype}/{reference_name}"
-
-        # Split the base URL and path
         base_url, path = original_url.split(f"/app/{reference_doctype}/", 1)
-
-        # Encode the path
         encoded_path = quote(path)
+        final_url = f"{base_url}/app/{reference_doctype.lower()}/{encoded_path}"
 
-        # Construct the final URL
-        final_url = f"{base_url}/app/item/{encoded_path}"
+        message_json = {
+                "to": "{0}".format(number),
+                "recipient_type": "individual",
+                "type": "template",
+                "template": {
+                    "language": {
+                        "policy": "deterministic",
+                        "code": "en_US"
+                    },
+                    "name": "lead_assignment",
+                    "components": [
+                        {
+                            "type": "body",
+                            "parameters": [
+                                {
+                                    "type": "text",
+                                    "text": "{0}".format(self.reference_name)
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "{0}".format(frappe.db.get_value("User", self.allocated_to, "full_name"))
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "{0}".format(self.reference_type)
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "{0}".format(final_url)
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        
+        url = wh_setting.url
+        token = wh_setting.token
+        phone_id = wh_setting.phone_id
+        version = wh_setting.version
+
+        end_point_url = url  + "/" + version + "/" + phone_id + "/" + "messages"
+
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json",
+        }
+        try:
+            # Sending the POST request
+            response = requests.post(end_point_url, headers=headers, json=message_json)
+
+            # Check the response status
+            if response.status_code == 200:
+                print("Request was successful!")
+                print("Response data:", response.json())
+            else:
+                frappe.log_error(response)
+        except requests.exceptions.RequestException as e:
+            frappe.log_error(e)
+        
+
+
+
+def validate_phone_number(number, allocated_to):
+    if len(number) == 10:
+        return "91" + number
+    if len(number) == 12 and number[0:2] == "91":
+        return number
+    frappe.throw("Please update the valid phone number in User {0}".format(get_link_to_form("User", allocated_to)))
 
     
 def notify_user(self):
---
+++ gettaapp/getta/doctype/whatsapp_setting/whatsapp_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, viral and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Whatsapp Setting", {
+// 	refresh(frm) {
+
+// 	},
+// });
